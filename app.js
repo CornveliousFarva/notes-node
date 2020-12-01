7bc4ed84035ee0289b9a55295ebf204cc36df12a
Greg Schuman <gcschuman@hotmail.com>
2020-11-30 21:22:36 -0800

    Deleted one of the chalk console logs.

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const chalk = require('chalk')
 const msg = getNotes()
 console.log(msg)
 
-console.log(chalk.green('Success!'))
+console.log(chalk.red('Error!'))
 console.log(chalk.dim('Fading'))
-console.log(chalk.bgYellowBright('I like that the Chargers moved to LA'))
 
