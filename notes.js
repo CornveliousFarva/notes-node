ccb73c0eac07d907c14e209e35ef743895d7197d
Greg Schuman <gcschuman@hotmail.com>
2020-12-08 19:13:38 -0800

    Added duplicate notes function.

--- notes.js
+++ notes.js
@@ -7,12 +7,23 @@ const getNotes = function () {
 //In the parenthesis, you call the arguments, in this case the title and body
 const addNote = function(title, body){
     const notes = loadNotes()
-    notes.push({
+    const duplicateNotes = notes.filter(function (note){
+        return note.title === title
+    })
+
+if(duplicateNotes.length === 0){
+   notes.push({
         title: title,
         body:body
     })
 
-    saveNotes(notes)
+    saveNotes(notes) 
+    console.log("New note added.")
+} else{
+    console.log("Note title taken")
+}
+
+    
 }
 
 const saveNotes = function(notes){
